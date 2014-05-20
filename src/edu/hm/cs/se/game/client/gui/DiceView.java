7e5d2280f24c0571451b6f050db6d84243872370
chaena <long.myan@gmail.com>
2014-05-20 16:45:30 +0200

    asdf +

--- src/edu/hm/cs/se/game/client/gui/DiceView.java
+++ src/edu/hm/cs/se/game/client/gui/DiceView.java
@@ -42,6 +42,7 @@ public class DiceView extends JPanel implements Observer {
      */
     private int index = 1;
 
+    //ADSFASDF
     /**
      * Custom-Constructor. Initializes the panel and its components.
      *
