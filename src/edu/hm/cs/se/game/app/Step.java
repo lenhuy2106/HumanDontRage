9c360faf78063ad4a6c4c87d8267bdce96c7fe95
lenhuy2106 <lenhuy.priv@googlemail.com>
2014-05-21 19:26:52 +0200

    created class 'step' depending on 'player'. should implement logic for issues #1 - #17

---
+++ src/edu/hm/cs/se/game/app/Step.java
@@ -0,0 +1,29 @@
+/*
+ * (C) Nhu-Huy Le, nle@hm.edu
+ * Oracle Corporation Java 1.8.0
+ * Microsoft Windows 7 Professional
+ * 6.1.7601 Service Pack 1 Build 7601
+ * This program is created while attending the courses 
+ * at Hochschule Muenchen Fak07, Germany in SS14.
+ */
+
+package edu.hm.cs.se.game.app;
+
+/**
+ *
+ * @author Nhu Huy Le - nle@hm.edu
+ */
+public class Step {
+    /** reference to current player */
+    private Player player;
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+    
+    public void doStep() {
+        
+    }
+    
+    
+}
