93bd636b58e12e65414890d461c54173f728f825
lenhuy2106 <lenhuy.priv@googlemail.com>
2014-05-23 20:03:15 +0200

    added mouseListener for fieldView

--- src/edu/hm/cs/se/game/client/gui/FieldView.java
+++ src/edu/hm/cs/se/game/client/gui/FieldView.java
@@ -74,6 +74,7 @@ public class FieldView extends JPanel implements MouseListener {
         this.field = field;
         setPreferredSize(new Dimension(40, 40));
         setBackground(Color.WHITE);
+        addMouseListener(this);    // essential line
     }
 
     @Override
