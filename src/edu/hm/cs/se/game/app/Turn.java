df2536dae4c6596a706a95ebcaf2678757a15c5a
lenhuy2106 <lenhuy.priv@googlemail.com>
2014-05-23 19:21:07 +0200

    optimized progress()

--- src/edu/hm/cs/se/game/app/Turn.java
+++ src/edu/hm/cs/se/game/app/Turn.java
@@ -22,21 +22,25 @@ public class Turn {
      * relevant state current inhabited  1: player may roll
                                          2: startfield is occupied, player may roll
                                          3: player may move
+                                         4: end
      */
     private int state;
-    private final int dice;
     private int attemptsLeft;
 
     public Turn(Player player) {
 
         this.player = player;
-        dice = player.getGame().getDice();
         state = 1;
         attemptsLeft = 3;
     }
-
+    /**
+     * Action progressing per turn of a player.
+     *
+     * @return true, if player is finished and player is changing.
+     */
     public boolean progress() {
 
+        int dice = player.getGame().getDice();
         boolean nextPlayer = false;
         switch (state) {
 
@@ -56,7 +60,7 @@ public class Turn {
                 } else if (dice == 6) {
                     if  (attemptsLeft >= 1) {
                         state = 2;                  // row 3
-                                                    // TODO: pawnAtHome &&
+                        player.start();             // TODO: pawnAtHome &&
                                                     //       startFieldFree
                     } else if (false) {
                         ;                           // row 8
@@ -64,27 +68,32 @@ public class Turn {
                                                     //       pawnMayMove
                     }
                 }
+                break;
             case 2:
                 if (dice < 6) {
                     if (false) {                    // row 4
                                                     // TODO: startPawnMayMove
-                        ;
+                        state = 1;
                     } else if (dice == 6) {
-                        ;                           // row 5
+                        state = 1;                  // row 5
                     } else if (dice <= 6) {
+                        state = 1;
                         ;                           // row 6
                     }
                 }
+                break;
             case 3:
                 if (false) {
                     ;                               // row 9
                 } else if (false) {
                     ;                               // row 10
                 }
+                break;
+            case 4:
+                break;
             default:
-                return nextPlayer;
+                break;
         }
-
+        return nextPlayer;
     }
-
 }
