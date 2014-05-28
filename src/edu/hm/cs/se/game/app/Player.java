f44e407c8b1e55ed2973f9ba2030c0d581b89214
chaena <long.myan@gmail.com>
2014-05-28 20:25:27 +0200

    Getter and Setter for endFields

--- src/edu/hm/cs/se/game/app/Player.java
+++ src/edu/hm/cs/se/game/app/Player.java
@@ -137,4 +137,16 @@ public class Player {
             }
 	}        
     }
+    
+    public boolean occupiedQ(int index){
+        return endFields.get(index - 1) != null;
+    }
+    
+    public void sendToEnd(int index){
+        endFields.get(index - 1).setPawn(new Pawn(index));
+    }
+    
+    public Field getStart(){
+        return startField;
+    }
 }
