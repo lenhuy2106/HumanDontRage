fe087e690ebd81934fb05c0f2dca9c8be536bb00
lenhuy2106 <lenhuy.priv@googlemail.com>
2014-05-29 23:58:51 +0200

    test

--- src/edu/hm/cs/se/game/client/gui/Main.java
+++ src/edu/hm/cs/se/game/client/gui/Main.java
@@ -58,7 +58,6 @@ public class Main extends JFrame {
                     frame.add(new DiceView(game), BorderLayout.NORTH);
                     frame.add(new BoardView(game, board), BorderLayout.CENTER);
                     frame.setVisible(true);
-                    System.out.println(game.toString());
 
                 } catch (Exception e) {
                     e.printStackTrace();
