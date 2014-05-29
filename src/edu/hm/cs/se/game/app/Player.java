a1bf8dedd2df40e5936849f36db27d5d515c1fab
chaena <long.myan@gmail.com>
2014-05-29 21:09:13 +0200

    Created Getter for Index of Player

--- src/edu/hm/cs/se/game/app/Player.java
+++ src/edu/hm/cs/se/game/app/Player.java
@@ -145,4 +145,8 @@ public class Player {
     public void sendToEnd(int endID){
         endFields.get(endID).setPawn(new Pawn(index));
     }
+    
+    public int getID(){
+        return index;
+    }
 }
